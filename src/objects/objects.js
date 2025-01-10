283d27acfc1b0c404b34e673400c9879b347d022
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2025-01-10 15:46:07 +0100

    exercise 'objects' solved

--- src/objects/objects.js
+++ src/objects/objects.js
@@ -5,12 +5,18 @@
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#objects
  */
 
+import { log } from "console"
+
 /**
  * @return {Object} you should return a new object containing two properties, first_name and last_name.
  * The values of the properties should be "Toto", "Tutu"
  */
 export function crateUserObject() {
-  // Write your code here
+  let person = {
+    first_name: "Toto",
+    last_name: "Tutu"
+  }
+  return person
 }
 
 /**
@@ -19,7 +25,7 @@ export function crateUserObject() {
  * @return string a concatenation of the first and last name, separated with a space
  */
 export function accessPropertiesInObjects(object) {
-  // Write your code here
+  return object.first_name + " " + object.last_name
 }
 
 /**
@@ -30,16 +36,62 @@ export function accessPropertiesInObjects(object) {
  * and all original object values mapped to lower case
  */
 export function iteratesThroughObjectValuesAndProperties(object) {
-  // Write your code here
+  const upperKeys = Object.keys(object)
+  const lowerValues = Object.values(object)
+
+  let toUpper = function (x) {
+    return x.toUpperCase();
+  }
+
+  let toLower = function (x) {
+    return x.toLowerCase();
+  }
+
+  const lowerValues2 = lowerValues.map(toLower)
+
+  const upperKeys2 = upperKeys.map(toUpper)
+
+  const newObject = {
+    keys: upperKeys2,
+    values: lowerValues2
+  }
+  
+  return newObject
 }
 
 /**
  * Return the name of the first younger and last older user in the array
- * @param {array<{name: string, age: number}>} users
+ * @param {Array<{name: string, age: number}>} users
  * @return {{younger: string, older: string}}
  */
 export function retrieveMaximumMinimumUserAges(users) {
-  // Write your code here
+  let min = Math.min(...users.map(item => item.age));
+  let max = Math.max(...users.map(item => item.age));
+
+  let nameMin = ""
+  let nameMax = ""
+  for (const object of users) {
+    if (object.age === min) {
+      nameMin = object.name
+      break
+    }
+  for (var i = users.length; i >= 0; i--) {
+    for ( const object of users) {
+      if (object.age === max) {
+        nameMax = object.name
+      }
+  }
+}
+    if (object.age === max) {
+      nameMax = object.name
+  }
+}
+  const MinMax = {
+    younger: nameMin,
+    older: nameMax
+  }
+  console.log(MinMax)
+  return MinMax
 }
 
 /**
@@ -49,7 +101,8 @@ export function retrieveMaximumMinimumUserAges(users) {
  * @return {Object} An object retried by parsing the string
  */
 export function parseJavaScriptObjectNotation(string) {
-  // Write your code here
+  let objString = JSON.parse(string)
+  return objString
 }
 
 /**
@@ -57,5 +110,6 @@ export function parseJavaScriptObjectNotation(string) {
  * @return {string} An string representing the given object
  */
 export function stringifyJavaScriptObjectNotation(object) {
-  // Write your code here
+  let stringObj = JSON.stringify(object)
+  return stringObj
 }
